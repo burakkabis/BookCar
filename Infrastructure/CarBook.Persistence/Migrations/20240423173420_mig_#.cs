b2bf7b0a3fd72aa4cecf324ccce3df1a44a8f6f1
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-04-23 20:35:47 +0300

    Add project files.

---
+++ Infrastructure/CarBook.Persistence/Migrations/20240423173420_mig_#.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "CarPricings",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Amount",
+                table: "CarPricings",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
