1c5ea315e789e23bb7aeb6227c5e2da8dc9aaa18
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-04-24 15:40:50 +0300

    24/4/2024

---
+++ Core/BookCar.Application/Features/CQRS/Results/AboutResults/GetAboutQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCar.Application.Features.CQRS.Results.AboutResults
+{
+    //About sinifinda listeleme islemi yaparken gelmesini istedigimiz propertyleri buraya yaziyoruz.
+    public class GetAboutQueryResult
+    {
+        public int AboutID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
