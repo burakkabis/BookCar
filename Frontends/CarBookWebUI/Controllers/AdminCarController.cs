16b6cd2e03ccc4d76fdc1a81f637e523982bbeb2
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-05-20 23:04:49 +0300

    20.05.2024 Commit 2

--- Frontends/CarBookWebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBookWebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using CarBook.Dto.CarDtos;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace CarBookWebUI.Controllers
@@ -21,6 +23,23 @@ namespace CarBookWebUI.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
                 return View(values);
             }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7164/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues = (from x in values
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.name,
+                                                    Value = x.brandID.ToString()
+                                                }).ToList();
+            ViewBag.BrandValues = brandValues;
             return View();
         }
     }
