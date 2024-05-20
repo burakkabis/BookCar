2949788653a8e427e61f86e35befabf3a99f9026
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-05-20 17:56:24 +0300

    20.05.2024

---
+++ Frontends/CarBookWebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookWebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7164/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
