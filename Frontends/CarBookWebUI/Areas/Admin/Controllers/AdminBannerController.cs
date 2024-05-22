329f957c7cbe90c25f4ff7c22a06210723e5999b
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-05-22 18:25:56 +0300

    22.05.2024

---
+++ Frontends/CarBookWebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -0,0 +1,52 @@
+﻿using CarBook.Dto.BannerDtos;
+using CarBook.Dto.BrandDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBookWebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBannerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminBannerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7164/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+       // [Route("CreateBanner")]
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+
+        [HttpPost]
+      //  [Route("CreateBanner")]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBannerDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7060/api/Banners", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
