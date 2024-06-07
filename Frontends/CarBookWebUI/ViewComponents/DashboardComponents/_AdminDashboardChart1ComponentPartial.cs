cbc59b022ad016e66d70951ab1235d4d44ed5f8b
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-06-07 16:26:35 +0300

    07.06.2024

---
+++ Frontends/CarBookWebUI/ViewComponents/DashboardComponents/_AdminDashboardChart1ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardChart1ComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBookWebUI/ViewComponents/DashboardComponents/_AdminDashboardChart3ComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardChart3ComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
