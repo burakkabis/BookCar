f7887879342be4650acf599e050bd9b9e808ab6f
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-06-09 21:44:56 +0300

    09.06.2024

---
+++ Frontends/CarBookWebUI/ViewComponents/CarDetailViewComponents/_CarDetailTabPaneComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.ViewComponents.CarDetailViewComponents
+{
+    public class _CarDetailTabPaneComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
