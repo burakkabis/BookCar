c29bed8a33aed376d6f59c0187259949efdd4094
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-06-13 16:02:17 +0300

    13.06.2024

---
+++ Frontends/CarBookWebUI/Controllers/SignalRCarController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.Controllers
+{
+    public class SignalRCarController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Presentation/BookCar.WebApi/Program.cs
+++ Presentation/BookCar.WebApi/Program.cs
@@ -17,6 +17,7 @@ using BookCar.Application.Interfaces.StatisticsInterfaces;
 using BookCar.Application.Interfaces.TagCloudInterfaces;
 using BookCar.Application.Services;
 using BookCar.Application.Tools;
+using BookCar.WebApi.Hubs;
 using CarBook.Persistence.Context;
 using CarBook.Persistence.Repositories;
 using CarBook.Persistence.Repositories.BlogRepositories;
@@ -36,6 +37,20 @@ using System.Reflection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
 {
@@ -130,10 +145,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<CarHub>("/carhub");
+
 
 app.Run();
